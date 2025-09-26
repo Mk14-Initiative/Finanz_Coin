574879031ab210d7b79cbf3f75226bfae4f434dc
Mk14-Initiative <marcel.raehse-bussines@gmx.de>
2025-09-26 20:39:30 +0200

    In Monatsübersicht Label eingefügt

--- monats_uebersicht.py
+++ monats_uebersicht.py
@@ -28,6 +28,12 @@ def month_window(parent_window):
 
     monats_window.columnconfigure(1, weight = 1)
 
+    monats_window.columnconfigure(2, weight = 1)
+
+    monats_window.columnconfigure(3, weight = 1)
+
+    monats_window.columnconfigure(4, weight = 1)
+
     monats_window.rowconfigure(0, weight = 1)
 
     monats_window.rowconfigure(1, weight = 1)
@@ -36,7 +42,27 @@ def month_window(parent_window):
 
     back_button = tk.Button(monats_window, text = "Zurück", command =lambda:main_window.start_window(monats_window))
 
+    year_label = tk.Label(monats_window, text = "Jahr")
+
+    month_label = tk.Label(monats_window, text = "Monat")
+
+    new_month_label = tk.Label(monats_window, text = "Neuer Monat")
+
+    delete_month_label = tk.Label(monats_window, text = "Monat Löschen")
+
+    sync_labe = tk.Label(monats_window, text = "Sync")
+
     back_button.grid(column = 0, row = 1, sticky = "nsew")
+
+    year_label.grid(column = 0, row = 0, sticky = "nsew")
+
+    month_label.grid(column = 1, row = 0, sticky = "nsew")
+
+    new_month_label.grid(column = 2, row = 0, sticky = "nsew")
+
+    delete_month_label.grid(column = 3, row = 0, sticky = "nsew")
+
+    sync_labe.grid(column = 4, row = 0, sticky = "nsew")
 
     close_button = tk.Button(monats_window, text="Schließen", command=monats_window.destroy)
 
